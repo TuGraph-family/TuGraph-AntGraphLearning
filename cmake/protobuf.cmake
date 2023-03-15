6ea0255b2e6583aeffbcd2b9a472f9f4d37abc3c
xianzheng.sxz <xianzheng.sxz@antgroup.com>
2023-03-15 19:35:31 +0800

    fix cmake

--- cmake/protobuf.cmake
+++ cmake/protobuf.cmake
@@ -1,5 +1,11 @@
+include(ExternalProject)
 ExternalProject_Add(protobuf
-        PREFIX ${CMAKE_CURRENT_SOURCE_DIR}/third_party/output/protobuf
-        SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party/protobuf
-        CMAKE_ARGS -DCMAKE_CXX_FLAGS="-Dprotobuf=${PROTOBUF_NS}" -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON
+        PREFIX ${PROJECT_SOURCE_DIR}/third_party/output/protobuf
+        SOURCE_DIR ${PROJECT_SOURCE_DIR}/third_party/protobuf
+        SOURCE_SUBDIR cmake
+        CMAKE_ARGS -DCMAKE_CXX_FLAGS="-Dprotobuf=${PB_NS}"
+                   -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON
+                   -Dprotobuf_BUILD_TESTS=OFF
+                   -DCMAKE_INSTALL_PREFIX:PATH=${PROJECT_SOURCE_DIR}/third_party/output/protobuf
         )
+install()
