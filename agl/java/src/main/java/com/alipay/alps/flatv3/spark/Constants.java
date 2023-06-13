d45673926aa5be3360aac441b09a6eefa3633b30
zhiyang.hzhy <zhiyang.hzhy@antfin.com>
2023-06-13 15:01:39 +0800

    add spark package

---
+++ agl/java/src/main/java/com/alipay/alps/flatv3/spark/Constants.java
@@ -0,0 +1,35 @@
+package com.alipay.alps.flatv3.spark;
+
+public class Constants {
+    public static final String NODE = "node";
+    public static final String EDGE = "edge";
+    public static final String ROOT = "root";
+    public static final String LIST_STR = "array<string>";
+    public static final String LIST_FLOAT = "array<float>";
+    public static final String LIST_DOUBLE = "array<double>";
+    public static final String LIST_LONG = "array<bigint>";
+
+    public static final String D2_PROJECT = "d2_project";
+    public static final String SEED_TABLE = "seed_table";
+    public static final String NEIGHBORS_TABLE = "neighbors_table";
+    public static final String NODE_TABLE = "node_table";
+    public static final String EDGE_TABLE = "edge_table";
+    public static final String RESULT_TABLE = "result_table";
+    public static final String INDEX_METAS = "index_metas";
+    public static final String FILTER_COND = "filter_cond";
+    public static final String SAMPLE_COND = "sample_cond";
+    public static final String OTHER_OUTPUT_SCHEMA = "other_output_schema";
+    public static final String HOP = "hop";
+    public static final String SUBGRAPH_SPEC = "subgraph_spec";
+    public static final String OUTPUT_TABLE = "output_table";
+    public static final String HAS_TYPE = "has_type";
+    public static final String HAS_EDGE_WEIGHT = "has_edge_weight";
+    public static final String INPUT_LABEL = "input_label";
+    public static final String INPUT_LINK = "input_link";
+    public static final String INPUT_NEIGHBOR = "input_neighbor";
+    public static final String INPUT_EDGE = "input_edge";
+    public static final String INPUT_FEATURE = "input_feature";
+    public static final String OUTPUT_RESULTS = "output_results";
+    public static final String INPUT_NODE_FEATURE = "input_node_feature";
+    public static final String INPUT_EDGE_FEATURE = "input_edge_feature";
+}
